8ec62390dcc5f4ec68a983d273bd4c0dc418078a
Dinesh <dineshdharavath03@gmail.com>
2026-05-24 08:48:50 +0530

    hooks pages

--- frontend/src/redux/jobSlice.js
+++ frontend/src/redux/jobSlice.js
@@ -1,16 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
 const jobSlice  = createSlice({
     name: 'job',
     initialState: {
         jobs: [],
+        adminJobs: [],
         loading: false,
         error: null,
     },
     reducers: {
         setAllJobs: (state, action) => {
             state.jobs = action.payload;
+        },
+        setAllAdminJobs: (state, action) => {
+            state.adminJobs = action.payload;
         }
     }
 });
 
-export const { setAllJobs } = jobSlice.actions;
+export const { setAllJobs, setAllAdminJobs } = jobSlice.actions;
 export default jobSlice.reducer;
